2182a1e644ae33bc129381d931bab8ed7025b0df
Vitalik <ztx25sss@gmail.com>
2020-11-12 09:53:13 +0200

    Updates

---
+++ precache-manifest.ceae4b908399ebb165f22a54fb38cc55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "246a47a9efe40b8734efb5bdad47d016",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d3001c57dc47f962f721",
+    "url": "./static/css/2.92730f33.chunk.css"
+  },
+  {
+    "revision": "f60286b2846ccac58b88",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "d3001c57dc47f962f721",
+    "url": "./static/js/2.b06b1596.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.b06b1596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f60286b2846ccac58b88",
+    "url": "./static/js/main.6f42008e.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
